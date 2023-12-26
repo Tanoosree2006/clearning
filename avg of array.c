4ca716e90b3d4683c47aa9326ea4faa8a46f24c3
Tanoosree2006 <147473260+Tanoosree2006@users.noreply.github.com>
2023-12-26 11:12:38 +0530

    Add files via upload

---
+++ avg of array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100];
+    float avg,sum =0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&arr[100]);
+        sum = sum+arr[100];
+    }
+    avg = sum/n;
+        printf("%.2f",avg);
+}
---
+++ sum of even positions in array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100],sum =0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[100]);
+        if(i%2==0)
+        {
+            sum = sum+arr[100];
+        }
+    }
+    {
+    	printf("%d",arr[100]);
+	}
+}
+    
